17f3149434e5d13ef8dd8f4d3ccdf89d9059eb7e
Letticia Nicoli <letticia.nicoli@gmail.com>
2016-08-13 13:43:22 -0300

    calibrar LDR

---
+++ calibrarLDR.ino
@@ -0,0 +1,13 @@
+const int LDR = 0;
+int valorLido = 0;
+
+void setup(){
+Serial.begin(9600);
+}
+
+void loop(){
+valorLido = analogRead(LDR);
+Serial.print("Valor lido pelo LDR: ");
+Serial.println(valorLido);
+delay(500);
+}
