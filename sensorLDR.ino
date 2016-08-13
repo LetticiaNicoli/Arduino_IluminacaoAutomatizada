0a0652711620ed9f0a553396bdf2e80431db6511
Letticia Nicoli <letticia.nicoli@gmail.com>
2016-08-13 13:42:57 -0300

    Código sensor de luminosidade

---
+++ sensorLDR.ino
@@ -0,0 +1,26 @@
+const int LDR = 0;
+const int Led = 6;
+int valorLido = 0;
+int pwm = 0;
+
+void setup(){
+  pinMode(Led,OUTPUT);
+}
+
+void loop(){
+  valorLido = analogRead(LDR);
+  
+  if(valorLido < 300){
+    analogWrite(Led,pwm);
+    pwm++;
+    delay(100);
+  }
+  else{
+    digitalWrite(Led,LOW);
+    pwm=0;
+  }
+  
+  if(pwm > 255){
+    pwm = 255;
+  }
+}
